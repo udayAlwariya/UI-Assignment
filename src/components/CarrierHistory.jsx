bb8bb0957df1ce882d5bc2e661b2a4d1c890545e
udayAlwariya <ravialwariya76@gmail.com>
2025-09-20 20:46:10 +0530

    Added text in carrier history topbar

--- src/components/CarrierHistory.jsx
+++ src/components/CarrierHistory.jsx
@@ -7,10 +7,27 @@ function CarrierHistory() {
   return (
     <div>
       <div className=" pl-[16px] bg-white pt-[16px] pb-[8px] mr-[16px] rounded-tl-2xl rounded-tr-2xl">
-        <h1 className="font-semibold">Load Details</h1>
+        <div className="flex justify-between">
+          <h1 className="font-semibold">Load Details</h1>
+          <div className="flex text-[13px] gap-[16px] pr-[14px]">
+            <div>
+            <span>On Time:</span>
+            <span className="font-semibold">5</span>
+          </div>
+          <div>
+            <span>Canceled:</span>
+            <span className="font-semibold">2</span>
+          </div>
+          <div>
+            <span>Total Revenue:</span>
+            <span className="font-semibold">$5,65,222.50</span>
+          </div>
+          </div>
+          
+        </div>
       </div>
       <DataTable rows={rows} name={"carrier"} />
-      <LogNotes/>
+      <LogNotes />
     </div>
   );
 }
